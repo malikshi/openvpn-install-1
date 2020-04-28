0581494b92730bc97cc75511cb42d00b00172316
Malik Al <kidznightserver@gmail.com>
2020-04-29 00:15:03 +0700

    remove openssl from apt

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -652,18 +652,18 @@ function installOpenVPN() {
 				apt-get update
 			fi
 			# Ubuntu > 16.04 and Debian > 8 have OpenVPN >= 2.4 without the need of a third party repository.
-			apt-get install -y openvpn iptables openssl wget ca-certificates curl
+			apt-get install -y openvpn iptables wget ca-certificates curl
 		elif [[ $OS == 'centos' ]]; then
 			yum install -y epel-release
-			yum install -y openvpn iptables openssl wget ca-certificates curl tar 'policycoreutils-python*'
+			yum install -y openvpn iptables wget ca-certificates curl tar 'policycoreutils-python*'
 		elif [[ $OS == 'amzn' ]]; then
 			amazon-linux-extras install -y epel
-			yum install -y openvpn iptables openssl wget ca-certificates curl
+			yum install -y openvpn iptables wget ca-certificates curl
 		elif [[ $OS == 'fedora' ]]; then
-			dnf install -y openvpn iptables openssl wget ca-certificates curl policycoreutils-python-utils
+			dnf install -y openvpn iptables wget ca-certificates curl policycoreutils-python-utils
 		elif [[ $OS == 'arch' ]]; then
 			# Install required dependencies and upgrade the system
-			pacman --needed --noconfirm -Syu openvpn iptables openssl wget ca-certificates curl
+			pacman --needed --noconfirm -Syu openvpn iptables wget ca-certificates curl
 		fi
 		# An old version of easy-rsa was available by default in some openvpn packages
 		if [[ -d /etc/openvpn/easy-rsa/ ]]; then
